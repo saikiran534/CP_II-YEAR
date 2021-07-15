c763ba918ca6efd0580e6c54d6219231b334d642
Vemula Saikiran <71496561+saikiran534@users.noreply.github.com>
2021-07-15 23:05:35 +0530

    pascal completed

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -7,6 +7,14 @@
 
 
 
+def factorial(n):
+	b = 1
+	for a in range(1,n+1):
+
+		b=b*a
+	return b
+
 
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	a = factorial(row)/(factorial(col)*(factorial(row-col)))
+	return int(a)
