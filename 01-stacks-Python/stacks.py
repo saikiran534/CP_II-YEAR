2c54cd9981db92b6bcebf98490ea145136dafe16
Vemula Saikiran <71496561+saikiran534@users.noreply.github.com>
2021-07-16 12:03:51 +0530

    stacks incomplete

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -25,20 +25,31 @@ class LinkedList(object):
             self.head = new_element
 
     def insert_first(self, new_element):
-        "Insert new element as the head of the LinkedList"
-        pass
+        current = self.head
+        if self.head:
+            new_element.next=self.head
+            self.head=new_element
+        else:
+            self.head = new_element
 
     def delete_first(self):
-        "Delete the first (head) element in the LinkedList as return it"
-        pass
-
+        self.head=self.head.next
+        
 class stack(object):
     def __init__(self,top=None):
         self.ll = LinkedList(top)
 
     def push(self, new_element):
-        "Push (add) a new element onto the top of the stack"
-        pass
+        
+        if self.head == None:
+            self.head=Element(new_element)
+        else:
+            new_element=Element(new_element)
+            new_element.next=self.head
+            self.head=new_element
+
+
+
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
