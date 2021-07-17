a2d8914ad0c271089f4a5fb66279f7bb03dd28e7
Vemula Saikiran <71496561+saikiran534@users.noreply.github.com>
2021-07-17 12:55:35 +0530

    Queue

--- 02-queues-Python/queues.py
+++ 02-queues-Python/queues.py
@@ -9,10 +9,13 @@ class Queue:
         self.storage = [head]
 
     def enqueue(self, new_element):
-        pass
+        self.storage.append(new_element)
+        
 
     def peek(self):
-        pass 
+        return self.storage[0]
 
     def dequeue(self):
-        pass
+        d =self.peek()
+        del self.storage[0]
+        return d
