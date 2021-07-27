2987779ded277b26cff2bc8caddc8f84d3ec61e7
Vemula Saikiran <71496561+saikiran534@users.noreply.github.com>
2021-07-27 10:54:28 +0530

    incomplete

--- 08-isrotated-Python/isrotated.py
+++ 08-isrotated-Python/isrotated.py
@@ -6,4 +6,20 @@
 
 def isrotated(str1, str2):
 	#Your code goes here
-	pass
+	x = [str(a) for a in str(str1)]
+	y = [str (a) for a in str(str2)]
+	if len(x)==len(y):
+		a = len(x)
+	
+	# print (len(x))
+	# print(x[::-1])
+		if y == x[::-1]:
+			return True
+		else:
+			return False
+	else:
+		return False
+	
+str1="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+str2 = "BCDEFGHIJKLMNOPQRSTUVWXYZA"
+print(isrotated(str1, str2))
