93be6182bb5b1482188035d1773ee2c2b2c771c6
Vemula Saikiran <71496561+saikiran534@users.noreply.github.com>
2021-08-02 14:43:40 +0530

    linked list done

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -53,32 +53,31 @@ class LinkedList(object):
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
         # Your code goes here
-        temp = self.head
-        count = 1
-        head = self.head
-        if (position ==1): 
-            new_element.next= head
-            head = new_element
-        else:
-            while (temp.head!= None and count < position-1): 
-                temp= temp.next
-                count =count+1
-                
-            new_element.next=temp.next
-            temp.next=new_element
+        counter=1
+        temp=self.head
+        if position>1:
+            while temp and counter<position:
+                if counter == position - 1:
+                    new_element.next = temp.next
+                    temp.next = new_element
+                temp = temp.next
+                counter += 1
+        elif position == 1:
+            new_element.next = self.head
+            self.head = new_element
 
     
     
     def delete(self, value):
         """Delete the first node with a given value."""
         # Your code goes here
-        temp=self.head
-        count = 1
-        if (self.head!=None): 
-            if (temp.value==value): 
-                self.head=self.head.next
-            else: 
-                while(temp.next.next!= None) and (temp.next.value!=value): 
-                    temp=temp.next
-                    count =count+1
-                temp.next = temp.next.next
+        temp = self.head
+        previous = None
+        while temp.value != value and temp.next:
+            previous = temp
+            temp = temp.next
+        if temp.value == value:
+            if previous:
+                previous.next = temp.next
+            else:
+                self.head = temp.next
